4359234d7c521d97e915edcc6e884156622ba481
mschuepbach <mschuepbach@pm.me>
2023-09-05 11:42:59 +0200

    Indicate when connection is closed

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
+	import { onDestroy, onMount } from 'svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	let webSocketEstablished = false;
 	let ws: WebSocket | null = null;
 	let items = data.items;
 	let newItem = '';
+	let showOffline = false;
 
 	const establishWebSocket = () => {
-		if (webSocketEstablished) return;
+		if (ws?.readyState === WebSocket.OPEN) return;
 		const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
 		ws = new WebSocket(`${protocol}//${window.location.host}/websocket`);
 		ws.addEventListener('open', (event) => {
-			webSocketEstablished = true;
 			console.log('[websocket] connection open', event);
+			showOffline = false;
 		});
 		ws.addEventListener('close', (event) => {
 			console.log('[websocket] connection closed', event);
+			showOffline = true;
+			establishWebSocket();
 		});
 		ws.addEventListener('message', (event) => {
 			console.log('[websocket] message received', event);
@@ -29,13 +31,18 @@
 		});
 	};
 
-	const addItem = async () => {
-		ws?.send(JSON.stringify({ operation: 'add', name: newItem }));
-		newItem = '';
+	const addItem = async (e: SubmitEvent) => {
+		e.preventDefault();
+		if (newItem !== '' && ws?.readyState === WebSocket.OPEN) {
+			ws?.send(JSON.stringify({ operation: 'add', name: newItem }));
+			newItem = '';
+		}
 	};
 
 	const removeItem = async (id: string) => {
-		ws?.send(JSON.stringify({ operation: 'remove', id }));
+		if (ws?.readyState === WebSocket.OPEN) {
+			ws?.send(JSON.stringify({ operation: 'remove', id }));
+		}
 	};
 
 	onMount(() => {
@@ -56,11 +63,20 @@
 		{/each}
 	</div>
 {:else}
-<div class="m-auto">
-	<p class="text-muted-foreground">No Items</p>
-</div>
+	<div class="m-auto">
+		<p class="text-muted-foreground">No Items</p>
+	</div>
 {/if}
-<div class="mt-auto flex w-full gap-2 p-2">
-	<Input class="w-full" bind:value={newItem} />
-	<Button on:click={() => addItem()}>Add</Button>
+<div class="mt-auto">
+	{#if showOffline}
+		<div class="flex h-6 w-full items-center justify-center bg-red-600 text-sm text-white">
+			You are offline
+		</div>
+	{/if}
+	<div class="w-full">
+		<form class="flex w-full gap-2 p-2" on:submit={(e) => addItem(e)}>
+			<Input class="w-full" bind:value={newItem} />
+			<Button>Add</Button>
+		</form>
+	</div>
 </div>
