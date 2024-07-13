b6f5fb791f56b4a8d1bf3c78522d9610f2da3637
mschuepbach <mschuepbach@pm.me>
2024-07-13 12:42:30 +0200

    Migration to Drizzle Kit 0.23.0

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,8 @@ dotenv.config();
 export default {
 	schema: './src/lib/server/schema.ts',
 	out: './drizzle',
-	driver: 'pg',
+	dialect: 'postgresql',
 	dbCredentials: {
-		connectionString: process.env.DB_URL ?? ''
+		url: process.env.DB_URL ?? ''
 	}
 } satisfies Config;
